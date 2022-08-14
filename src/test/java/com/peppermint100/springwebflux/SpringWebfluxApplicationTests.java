cdb791f40b1fc5ba6d2e2288a017160c990d7572
이인규 <ingyulee@tnear.com>
2022-08-14 10:12:46 +0900

    - Init With Required Dependencies

---
+++ src/test/java/com/peppermint100/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppermint100.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
